bc0cfa60eca2d0e30b0bc7b2a965bc9404f1ff1b
Marcel Wüthrich <marcel.wuthrich@wunet.ch>
2023-11-08 21:25:29 +0100

    Mise à jour

---
+++ class/class_tracking.php
@@ -0,0 +1,48 @@
+<?php
+
+class cl_tracking
+
+{
+
+    
+    public function getTrackingFromDatePerId($per_id, $trk_booking_date_time_from) {
+    
+        
+        // Connexion à la DB
+        $user='root';
+        $pass='root';
+
+        try {
+            $dbh = new PDO('mysql:host=localhost;dbname=vysual', $user, $pass);
+            //echo 'Connection opened<BR>';
+        }
+        catch (PDOException $e) {
+            // tenter de réessayer la connexion après un certain délai, par exemple
+            echo 'Error by opening connection<BR>';
+        }
+
+        try {
+            $sql  = 'select * from gbl_tracking where per_id = \'' . $per_id . '\' and date(trk_booking_date_time) = \'' . $trk_booking_date_time_from . '\' order by trk_id asc;';
+            $dbh->beginTransaction();
+            $sth = $dbh->query($sql);
+            $rows = $sth->fetchAll();
+            $dbh->rollBack();
+            //$sth = null;
+            //$dbh = nulll;
+        }
+        catch (PDOException $e) {
+            echo "Failed: " . $e->getMessage() . '<BR>';
+        }
+        
+        return $rows;
+    }
+
+    
+
+        
+
+
+}
+    
+
+?>
--- tracking_user_day.php
+++ tracking_user_day.php
@@ -8,23 +8,102 @@
 
 <body >
 
+<form action="tracking_user_day.php" method="post">
 
-
-<a href="tracking.php">Backup to tracking</a>
-<Br>
-<a href="index.php">Backup to my PHP Tools</a>
 
 <?php
 
 
-//include 'class_tracking.php';
-//include 'class_timecode.php';
-//include 'classe_person.php';
+include 'class/class_tracking.php';
 
+if (!empty($_POST)) {
+
+    $mytrk  = new cl_tracking;
+    $myresults = $mytrk->getTrackingFromDatePerId($_POST["per_id"],$_POST["this_date"]);
+
+    foreach($myresults as $myresult ){
+        echo $myresult["trk_id"] . ' ';
+        echo $myresult["trk_booking_date_time"] . ' ';
+
+        switch($myresult["trk_action"]) {
+
+            case 210:
+                echo 'Stop Activity';
+                break;
+    
+                case 240:
+                    echo 'Start Activity';
+                    break;
+        
+                    case 254:
+                        echo 'Out';
+                        break;
+            
+                        case 255:
+                            echo 'In';
+                            break;
+                
+                                        
+        }
+        echo '<BR>';
+        
+        //echo date_format($myresult["trk_booking_date_time"],"Y/m/d");
+        //echo date_format($myresult["trk_booking_date_time"],"H:i:s") . '<BR>';
+        
+    }
+
+}
+
+
+
+$_POST["per_id"] = '000001-20170904-0000000110';
+$_POST["this_date"] = '2023-07-14';
+
+echo '<label for="this_date" size="50">date:</label><BR>';
+if (empty($_POST["this_date"])) {
+    echo '<input type="date" id="this_date" name="this_date" value="' . date("Y-m-d") . '" />';
+}
+else {
+    echo '<input type="date" id="this_date" name="this_date" value="' . $_POST["this_date"] .  '" />';
+}
+echo '<BR><BR>';
+
+echo '<label for="per_id" size="50">per_id:</label><BR>';
+if (empty($_POST["per_id"])) {
+    echo '<input type="text" id="per_id" name="per_id" value="" size="30"><br><br>';
+}
+else {    
+    echo '<input type="text" id="per_id" name="per_id" value="' . $_POST["per_id"] . '" size="30"><br><br>';
+
+}
+
+
+/*
+
+if (!empty($_POST))   {
+    echo '<input type="date" id="this_date" name="this_date" value="' . $_POST["this_date"] .  '" />';
+}
+else {
+    // echo '<input type="date" id="this_date" name="this_date" value="' . date("Y-m-d") . '" />';
+    echo '<input type="date" id="this_date" name="this_date" value="2022-07-14" />';
+}
+
+*/
 
 
 ?>
+<BR><BR>
 
+
+
+<input type="submit" value="Execute">
+
+<BR><BR>
+<a href="tracking.php">Backup to tracking</a>
+<BR>
+<a href="index.php">Backup to my PHP Tools</a>
+
+</form>
 
 </body>
 </html>

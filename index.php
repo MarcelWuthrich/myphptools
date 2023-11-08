56f50d30101be8f51d8df0e7c161ae6d4f864b83
Marcel Wüthrich <marcel.wuthrich@wunet.ch>
2023-11-08 19:31:46 +0100

    Ajout de pages

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>my PHP Tools</title>
+</head>
+
+<body >
+
+<form action="tracking.php" method="post">
+
+
 <?php
-phpinfo();
+
+
+//include 'class_tracking.php';
+//include 'class_timecode.php';
+//include 'classe_person.php';
+
+
+
 ?>
+
+
+
+</body>
+</html>

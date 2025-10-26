70f03ecb6c224bef0f2678d0b7c541bd5cd7aebe
Marcel Wüthrich <marcel.wuthrich@wunet.ch>
2025-10-26 09:14:24 +0100

    Déplacement de fichiers Viteos oubliés

---
+++ files/insert_accounting.sql
@@ -0,0 +1,40 @@
+
+--   : Heures BAL
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',116640000,116640000,GetNextId(),'Ajout du solde de Heures BAL dans  : 32.40 heure(s)','admin',NOW());
+
+--   : Heures SUP
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',-38052000,-38052000,GetNextId(),'Ajout du solde de Heures SUP dans  : -10.57 heure(s)','admin',NOW());
+
+--   : Pont
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',76320000,76320000,GetNextId(),'Ajout du solde de Pont dans  : 21.20 heure(s)','admin',NOW());
+
+--   : Unité Piquet
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',180000000,180000000,GetNextId(),'Ajout du solde de Unité Piquet dans  : 50.00 heure(s)','admin',NOW());
+
+--   : Vacances
+
+--   : Heures BAL
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',692928000,692928000,GetNextId(),'Ajout du solde de Heures BAL dans  : 192.48 heure(s)','admin',NOW());
+
+--   : Heures SUP
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',147888000,147888000,GetNextId(),'Ajout du solde de Heures SUP dans  : 41.08 heure(s)','admin',NOW());
+
+--   : Pont
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',77040000,77040000,GetNextId(),'Ajout du solde de Pont dans  : 21.40 heure(s)','admin',NOW());
+
+--   : Unité Piquet
+
+--   : Vacances
+
+--   : Heures BAL
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',-468000,-468000,GetNextId(),'Ajout du solde de Heures BAL dans  : -0.13 heure(s)','admin',NOW());
+
+--   : Heures SUP
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',720000,720000,GetNextId(),'Ajout du solde de Heures SUP dans  : 0.20 heure(s)','admin',NOW());
+
+--   : Pont
+INSERT INTO vtm_activity_counter_accounting (aca_id,avc_id,per_id,tco_id,wkt_id,aca_date_time,aca_type,aca_amount,aca_real_amount,aca_series,aca_comment,aca_created_by,aca_created_date) VALUES (GetNextId(),'','','','','2024-08-31','USER',71280000,71280000,GetNextId(),'Ajout du solde de Pont dans  : 19.80 heure(s)','admin',NOW());
+
+--   : Unité Piquet
+
+--   : Vacances
